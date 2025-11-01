65e0c937112c395717698f07c9fe48c3cd2c6bbd
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2025-11-01 13:35:59 +0200

    add-products

--- src/app/core/interceptors/global.interceptor.ts
+++ src/app/core/interceptors/global.interceptor.ts
@@ -7,37 +7,12 @@ import { ApiEndPoint } from '../../../environments/ApiEndPoint';
 
 export const globalInterceptor: HttpInterceptorFn = (req, next) => {
 
-  // const baseUrl: string = environment.apiUrl;
+  const baseUrl: string = environment.apiUrl;
 
-  // // JUST TEST
-  // const TokenUser = TOKEN.User;
-  // const TokenAdmin = TOKEN.Admin;
+  // JUST TEST
+  const TokenUser = TOKEN.User;
+  const TokenAdmin = TOKEN.Admin;
 
-<<<<<<< HEAD
-  // let PLATFORM = inject(PLATFORM_ID);
-
-  // if (isPlatformBrowser(PLATFORM)) {
-  //   const token = localStorage.getItem('token') || '';
-
-  //   req = req.clone({
-  //     url: `${baseUrl}${req.url}`,
-  //     setHeaders: {
-  //       Authorization: `Bearer ${TokenUser}` 
-  //     }
-  //   });
-  // }
-
-  // return next(req);
-
-  // --------------------------
-  const baseUrl = environment.apiUrl;
-  const PLATFORM = inject(PLATFORM_ID);
-
-  if (isPlatformBrowser(PLATFORM)) {
-    // Get tokens
-    const userToken = localStorage.getItem('userToken') || '';
-    const adminToken = localStorage.getItem('adminToken') || '';
-=======
   let PLATFORM = inject(PLATFORM_ID);
   const normalUserEndpoints :string[]=[ApiEndPoint.LOW_STOCK_PRODUCTS,ApiEndPoint.STATISTICS_ORDERS,ApiEndPoint.TOP_SELLING_PRODUCTS];
   console.log(req.url);
@@ -61,26 +36,22 @@ export const globalInterceptor: HttpInterceptorFn = (req, next) => {
 
   else if (isPlatformBrowser(PLATFORM)) {
     const token = localStorage.getItem('token') || '';
->>>>>>> b1439bb2946d9746ced0415a5bddcc817aaf892f
 
-    let token = '';
-
-    // 🧠 Decide which token to use based on the request URL or endpoint
-    if (req.url.includes('/dashboard')) {
-      token = adminToken;
-    } else {
-      token = userToken;
-    }
-
-    // 🛠 Clone request and add token + base URL
     req = req.clone({
       url: `${baseUrl}${req.url}`,
-      setHeaders: token
-        ? {
-            Authorization: `Bearer ${token}`,
-          }
-        : {},
+      setHeaders: {
+        Authorization: `Bearer ${TokenUser}`
+      }
     });
+
+    /*if (token !== null) {
+      req = req.clone({
+        url: `${baseUrl}${req.url}`,
+        setHeaders: {
+          Authorization: `Bearer ${Token}`
+        }
+      });
+    }*/
   }
 
   return next(req);
